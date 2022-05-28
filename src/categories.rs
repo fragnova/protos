0a982e57e5865208d332ff543dffcc3670e87a04
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-28 17:34:45 +0800

    new categories and wires

--- src/categories.rs
+++ src/categories.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
-pub struct ChainTrait(Compact<u32>);
+pub struct WireTrait(Compact<u32>);
 
 // serde(rename_all = "camelCase") is needed or polkadot.js will not be able to deserialize
 
@@ -15,12 +15,11 @@ pub struct ChainTrait(Compact<u32>);
 pub enum AudioCategories {
 	/// A compressed audio file in the ogg container format
 	OggFile,
-	///
 	/// A compressed audio file in the mp3 format
 	Mp3File,
-	/// A chainblocks script that returns an effect chain that requires an input, validated
+	/// A shards script that returns an effect wire that requires an input, validated
 	Effect,
-	/// A chainblocks script that returns an instrument chain (no audio input), validated
+	/// A shards script that returns an instrument wire (no audio input), validated
 	Instrument,
 }
 
@@ -29,7 +28,7 @@ pub enum AudioCategories {
 pub enum ModelCategories {
 	/// A GLTF binary model
 	GltfFile,
-	/// ???
+	/// 🤫😄
 	Sdf,
 	/// A physics collision model
 	PhysicsCollider,
@@ -46,40 +45,49 @@ pub enum TextureCategories {
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
 pub enum VectorCategories {
 	SvgFile,
-	FontFile,
+	/// A TrueType font file
+	TtfFile,
 }
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
 pub enum VideoCategories {
+	MkvFile,
 	Mp4File,
 }
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
 pub enum TextCategories {
+	/// Plain Text
 	Plain,
+	/// Json String
 	Json,
 }
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
 pub enum BinaryCategories {
-	WasmModule,
+	/// A generic wasm program, compiled to run on a WASI runtime
+	WasmProgram,
+	/// A generic wasm reactor, compiled to run on a WASI runtime
+	WasmReactor,
+	/// A blender file. Royalties distribution of blender files derived protos will always allocate a % to the Blender Foundation
+	BlendFile,
 }
 
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize), serde(rename_all = "camelCase"))]
-pub enum ChainCategories {
-	/// A chainblocks script that returns a generic chain (we validate that)
+pub enum WireCategories {
+	/// A shards script that returns a generic wire (we validate that)
 	Generic,
-	/// An animation sequence in chainblocks edn
+	/// An animation sequence in shards edn
 	Animation,
-	/// A chainblocks script that returns a shader chain constrained to be a vertex shader (we validate that)
+	/// A shards script that returns a shader wire constrained to be a vertex shader (we validate that)
 	VertexShader,
-	/// A chainblocks script that returns a shader chain constrained to be a fragment shader (we validate that)
+	/// A shards script that returns a shader wire constrained to be a fragment shader (we validate that)
 	FragmentShader,
-	/// A chainblocks script that returns a shader chain constrained to be a compute shader (we validate that)
+	/// A shards script that returns a shader wire constrained to be a compute shader (we validate that)
 	ComputeShader,
 }
 
@@ -89,9 +97,11 @@ pub enum ChainCategories {
 pub enum Categories {
 	/// Text of the supported sub-categories
 	Text(TextCategories),
-	/// Chainblocks chains of various sub-categories
-	/// Chains also can have interoperability traits to describe how they can be used in other chains
-	Chain(ChainCategories, Vec<ChainTrait>),
+	/// A Scripting Trait declaration, traits are unique, and are used to describe how Wires work (Scripts)
+	Trait(WireTrait),
+	/// Shards scripts (Wires) of various sub-categories
+	/// Wires also can have interoperability traits to describe how they can be used in other wires
+	Wire(WireCategories, Vec<WireTrait>),
 	/// Audio files and effects
 	Audio(AudioCategories),
 	/// Textures of the supported sub-categories
