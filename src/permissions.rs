41a04b4236a69c99da1b1dd5a85c258e13bd0979
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2022-05-31 21:01:00 +0800

    expose fragments perms properly

--- src/permissions.rs
+++ src/permissions.rs
@@ -2,11 +2,31 @@ use bitflags::bitflags;
 
 bitflags! {
   /// Permissions for fragments and fragment's bundles.
-  struct FragmentPerms: u32 {
+  pub struct FragmentPerms: u32 {
     const NONE = 0;
     const EDIT = 1;
     const COPY = 2;
     const TRANSFER = 4;
     const ALL = Self::EDIT.bits | Self::COPY.bits | Self::TRANSFER.bits;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  struct TestStruct {
+    pub name: String,
+    pub permissions: FragmentPerms,
+  }
+
+  #[test]
+  fn t1() {
+    let test_struct = TestStruct {
+      name: "test".to_string(),
+      permissions: FragmentPerms::NONE,
+    };
+
+    assert_eq!(test_struct.permissions.bits, 0);
   }
 }
