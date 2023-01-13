827e9a3a05f5f85b2a8695a0db097a4fb7459bf5
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-01-13 11:54:01 +0800

    add safetensors

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ module.exports = {
                     "wasmReactor",
                     "blendFile",
                     "onnxModel",
+                    "safeTensors",
                 ]
             },
 
