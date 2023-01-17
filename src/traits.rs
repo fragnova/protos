a552bed241ef7d39255fc6027bb75c4481489b90
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-01-17 19:20:23 +0800

    make traits json serializable

--- src/traits.rs
+++ src/traits.rs
@@ -1,12 +1,16 @@
 use parity_scale_codec::{Decode, Encode};
 use scale_info::prelude::{boxed::Box, vec::Vec};
 
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+
 // For more info refer to:
 // https://github.com/fragcolor-xyz/shards/blob/devel/include/shards.h
 
 #[cfg(not(feature = "std"))]
 type String = Vec<u8>;
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub enum TriState {
     Either,
@@ -19,6 +23,7 @@ pub enum TriState {
 /// Note: There can only be 2 Code Types:
 /// 1. A Shard
 /// 2. A Wire
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub enum CodeType {
     /// A list of shards, to be injected into more complex blocks of code or wires
@@ -28,6 +33,7 @@ pub enum CodeType {
 }
 
 /// Struct represents the information about a Code (Note: There are only 2 Code Types: A Shard or a Wire. See the enum `CodeType` above to understand more)
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub struct CodeInfo {
     /// The Type of the Code (i.e the Code Type)
@@ -44,6 +50,7 @@ pub struct CodeInfo {
     pub output: VariableType,
 }
 
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub struct TableInfo {
     /// Tha name of the keys, an empty key represent any name, allowing multiple instances of the corresponding index type
@@ -53,6 +60,7 @@ pub struct TableInfo {
 }
 
 /// Enum represents all the possible types that a variable can be
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub enum VariableType {
     None,
@@ -103,9 +111,11 @@ pub enum VariableType {
 }
 
 /// Struct contains information about a variable type
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub struct VariableTypeInfo {
     /// The variable type
+    #[serde(alias = "type")]
     pub type_: VariableType,
     /// Raw-bytes representation of the default value of the variable type (optional)
     pub default: Option<Vec<u8>>,
@@ -113,6 +123,7 @@ pub struct VariableTypeInfo {
 
 /// TODO Review - Definition
 /// A Trait Attribute's Type
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub enum RecordInfo {
     SingleType(VariableTypeInfo),
@@ -120,6 +131,7 @@ pub enum RecordInfo {
 }
 
 /// Struct represents a Trait
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, Clone, PartialEq, Debug, Eq, scale_info::TypeInfo)]
 pub struct Trait {
     /// Name of the Trait
@@ -217,5 +229,113 @@ mod tests {
             _ => panic!("Expected a code"),
         };
         assert!(requires[0].0 == "int1".to_string());
+    }
+
+    #[test]
+    fn test_json_simple_1() {
+        let mut trait1 = vec![(
+            "int1".to_string(),
+            RecordInfo::SingleType(VariableTypeInfo {
+                type_: VariableType::Int,
+                default: None,
+            }),
+        )];
+
+        // THIS IS the way we reprocess the trait declaration before sorting it on chain and hashing it
+        trait1 = trait1
+            .into_iter()
+            .map(|(name, info)| (name.to_lowercase(), info))
+            .collect();
+        trait1.dedup_by(|a, b| a.0 == b.0);
+        trait1.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let trait1 = Trait {
+            name: "Trait1".to_string(),
+            records: trait1,
+        };
+
+        let json_trait1 = serde_json::to_string(&trait1).unwrap();
+
+        println!("json_trait1: {}", json_trait1);
+
+        let d_trait1 = serde_json::from_str(&json_trait1).unwrap();
+
+        assert!(trait1 == d_trait1);
+    }
+
+    #[test]
+    fn test_json_boxed_1() {
+        let mut trait1 = vec![
+            (
+                "int1".to_string(),
+                RecordInfo::SingleType(VariableTypeInfo {
+                    type_: VariableType::Int,
+                    default: None,
+                }),
+            ),
+            (
+                "boxed1".to_string(),
+                RecordInfo::SingleType(VariableTypeInfo {
+                    type_: VariableType::Code(Box::new(CodeInfo {
+                        kind: CodeType::Wire {
+                            looped: TriState::Either,
+                        },
+                        requires: vec![("int1".to_string(), VariableType::Int)],
+                        exposes: vec![],
+                        inputs: vec![],
+                        output: VariableType::None,
+                    })),
+                    default: None,
+                }),
+            ),
+        ];
+
+        // THIS IS the way we reprocess the trait declaration before sorting it on chain and hashing it
+        trait1 = trait1
+            .into_iter()
+            .map(|(name, info)| (name.to_lowercase(), info))
+            .collect();
+        trait1.dedup_by(|a, b| a.0 == b.0);
+        trait1.sort_by(|a, b| a.0.cmp(&b.0));
+
+        let trait1 = Trait {
+            name: "Trait1".to_string(),
+            records: trait1,
+        };
+
+        let json_trait1 = serde_json::to_string(&trait1).unwrap();
+
+        let d_trait1 = serde_json::from_str(&json_trait1).unwrap();
+
+        assert!(trait1 == d_trait1);
+        assert!(d_trait1.records[0].0 == "boxed1".to_string());
+        let requires = match d_trait1.records[0].1 {
+            RecordInfo::SingleType(VariableTypeInfo {
+                type_: VariableType::Code(ref code),
+                default: None,
+            }) => code.requires.clone(),
+            _ => panic!("Expected a code"),
+        };
+        assert!(requires[0].0 == "int1".to_string());
+    }
+
+    #[test]
+    fn test_json_textual_from_str() {
+        let trait1 = Trait {
+            name: "Trait1".to_string(),
+            records: vec![(
+                "int1".to_string(),
+                RecordInfo::SingleType(VariableTypeInfo {
+                    type_: VariableType::Int,
+                    default: None,
+                }),
+            )],
+        };
+
+        let json_trait1 = r#"{"name":"Trait1","records":[["int1",{"SingleType":{"type":"Int","default":null}}]]}"#;
+
+        let d_trait1 = serde_json::from_str(&json_trait1).unwrap();
+
+        assert!(trait1 == d_trait1);
     }
 }
