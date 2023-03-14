8a5c25a4bd46a212c5ffaadc1e7977f8fb9035c2
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-03-14 10:05:33 +0800

    smaller FragmentPerms and bump

--- src/permissions.rs
+++ src/permissions.rs
@@ -4,7 +4,7 @@ use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 bitflags! {
   /// Permissions for fragments and fragment's bundles.
   #[derive(Encode, Decode, MaxEncodedLen, scale_info::TypeInfo)]
-  pub struct FragmentPerms: u32 {
+  pub struct FragmentPerms: u8 {
     const NONE = 0;
     const EDIT = 1;
     const COPY = 2;
@@ -30,5 +30,45 @@ mod tests {
     };
 
     assert_eq!(test_struct.permissions.bits, 0);
+  }
+
+  #[test]
+  fn t2() {
+    let test_struct = TestStruct {
+      _name: "test".to_string(),
+      permissions: FragmentPerms::EDIT,
+    };
+
+    assert_eq!(test_struct.permissions.bits, 1);
+  }
+
+  #[test]
+  fn t3() {
+    let test_struct = TestStruct {
+      _name: "test".to_string(),
+      permissions: FragmentPerms::COPY,
+    };
+
+    assert_eq!(test_struct.permissions.bits, 2);
+  }
+
+  #[test]
+  fn t4() {
+    let test_struct = TestStruct {
+      _name: "test".to_string(),
+      permissions: FragmentPerms::TRANSFER,
+    };
+
+    assert_eq!(test_struct.permissions.bits, 4);
+  }
+
+  #[test]
+  fn t5() {
+    let test_struct = TestStruct {
+      _name: "test".to_string(),
+      permissions: FragmentPerms::ALL,
+    };
+
+    assert_eq!(test_struct.permissions.bits, 7);
   }
 }
